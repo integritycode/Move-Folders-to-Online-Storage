96779a9895f39abd413439557067393d387c0711
David Ruth <integritycode@gmail.com>
2017-01-14 08:30:54 -0600

    Add project files.

---
+++ GKMoveFolders/Form1.Designer.cs
@@ -0,0 +1,453 @@
+﻿namespace GKMoveFolders
+{
+	partial class frmMain
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.gboxDesktop = new System.Windows.Forms.GroupBox();
+			this.Browse = new System.Windows.Forms.Button();
+			this.lblDesktop = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.lblDocuments = new System.Windows.Forms.Label();
+			this.gboxDocuments = new System.Windows.Forms.GroupBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.lblDownloads = new System.Windows.Forms.Label();
+			this.gboxDownloads = new System.Windows.Forms.GroupBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.lblMusic = new System.Windows.Forms.Label();
+			this.gboxMusic = new System.Windows.Forms.GroupBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.lblPictures = new System.Windows.Forms.Label();
+			this.gboxPictures = new System.Windows.Forms.GroupBox();
+			this.button5 = new System.Windows.Forms.Button();
+			this.lblVideo = new System.Windows.Forms.Label();
+			this.gboxVideo = new System.Windows.Forms.GroupBox();
+			this.gboxDropbox = new System.Windows.Forms.GroupBox();
+			this.btnDropbox = new System.Windows.Forms.Button();
+			this.lblDropbox = new System.Windows.Forms.Label();
+			this.gboxOneDrive = new System.Windows.Forms.GroupBox();
+			this.btnOneDrive = new System.Windows.Forms.Button();
+			this.lblOneDrive = new System.Windows.Forms.Label();
+			this.btnMove = new System.Windows.Forms.Button();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.rdoDropbox = new System.Windows.Forms.RadioButton();
+			this.rdoOneDrive = new System.Windows.Forms.RadioButton();
+			this.gboxDropBoxOneDrive = new System.Windows.Forms.GroupBox();
+			this.lblOR = new System.Windows.Forms.Label();
+			this.gboxDesktop.SuspendLayout();
+			this.gboxDocuments.SuspendLayout();
+			this.gboxDownloads.SuspendLayout();
+			this.gboxMusic.SuspendLayout();
+			this.gboxPictures.SuspendLayout();
+			this.gboxVideo.SuspendLayout();
+			this.gboxDropbox.SuspendLayout();
+			this.gboxOneDrive.SuspendLayout();
+			this.gboxDropBoxOneDrive.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// gboxDesktop
+			// 
+			this.gboxDesktop.Controls.Add(this.Browse);
+			this.gboxDesktop.Controls.Add(this.lblDesktop);
+			this.gboxDesktop.Location = new System.Drawing.Point(13, 12);
+			this.gboxDesktop.Name = "gboxDesktop";
+			this.gboxDesktop.Size = new System.Drawing.Size(959, 62);
+			this.gboxDesktop.TabIndex = 0;
+			this.gboxDesktop.TabStop = false;
+			this.gboxDesktop.Text = "Desktop";
+			// 
+			// Browse
+			// 
+			this.Browse.Location = new System.Drawing.Point(878, 15);
+			this.Browse.Name = "Browse";
+			this.Browse.Size = new System.Drawing.Size(80, 40);
+			this.Browse.TabIndex = 1;
+			this.Browse.Text = "Browse";
+			this.Browse.UseVisualStyleBackColor = true;
+			this.Browse.Visible = false;
+			// 
+			// lblDesktop
+			// 
+			this.lblDesktop.AutoSize = true;
+			this.lblDesktop.Location = new System.Drawing.Point(7, 20);
+			this.lblDesktop.Name = "lblDesktop";
+			this.lblDesktop.Size = new System.Drawing.Size(91, 13);
+			this.lblDesktop.TabIndex = 0;
+			this.lblDesktop.Text = "Desktop Location";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(878, 15);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(80, 40);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Browse";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Visible = false;
+			// 
+			// lblDocuments
+			// 
+			this.lblDocuments.AutoSize = true;
+			this.lblDocuments.Location = new System.Drawing.Point(7, 20);
+			this.lblDocuments.Name = "lblDocuments";
+			this.lblDocuments.Size = new System.Drawing.Size(105, 13);
+			this.lblDocuments.TabIndex = 0;
+			this.lblDocuments.Text = "Documents Location";
+			// 
+			// gboxDocuments
+			// 
+			this.gboxDocuments.Controls.Add(this.button1);
+			this.gboxDocuments.Controls.Add(this.lblDocuments);
+			this.gboxDocuments.Location = new System.Drawing.Point(13, 80);
+			this.gboxDocuments.Name = "gboxDocuments";
+			this.gboxDocuments.Size = new System.Drawing.Size(959, 62);
+			this.gboxDocuments.TabIndex = 2;
+			this.gboxDocuments.TabStop = false;
+			this.gboxDocuments.Text = "Documents";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(878, 15);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(80, 40);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Browse";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Visible = false;
+			// 
+			// lblDownloads
+			// 
+			this.lblDownloads.AutoSize = true;
+			this.lblDownloads.Location = new System.Drawing.Point(7, 20);
+			this.lblDownloads.Name = "lblDownloads";
+			this.lblDownloads.Size = new System.Drawing.Size(104, 13);
+			this.lblDownloads.TabIndex = 0;
+			this.lblDownloads.Text = "Downloads Location";
+			// 
+			// gboxDownloads
+			// 
+			this.gboxDownloads.Controls.Add(this.button2);
+			this.gboxDownloads.Controls.Add(this.lblDownloads);
+			this.gboxDownloads.Location = new System.Drawing.Point(13, 148);
+			this.gboxDownloads.Name = "gboxDownloads";
+			this.gboxDownloads.Size = new System.Drawing.Size(959, 62);
+			this.gboxDownloads.TabIndex = 2;
+			this.gboxDownloads.TabStop = false;
+			this.gboxDownloads.Text = "Downloads";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(878, 15);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(80, 40);
+			this.button3.TabIndex = 1;
+			this.button3.Text = "Browse";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Visible = false;
+			// 
+			// lblMusic
+			// 
+			this.lblMusic.AutoSize = true;
+			this.lblMusic.Location = new System.Drawing.Point(7, 20);
+			this.lblMusic.Name = "lblMusic";
+			this.lblMusic.Size = new System.Drawing.Size(79, 13);
+			this.lblMusic.TabIndex = 0;
+			this.lblMusic.Text = "Music Location";
+			// 
+			// gboxMusic
+			// 
+			this.gboxMusic.Controls.Add(this.button3);
+			this.gboxMusic.Controls.Add(this.lblMusic);
+			this.gboxMusic.Location = new System.Drawing.Point(13, 216);
+			this.gboxMusic.Name = "gboxMusic";
+			this.gboxMusic.Size = new System.Drawing.Size(959, 62);
+			this.gboxMusic.TabIndex = 2;
+			this.gboxMusic.TabStop = false;
+			this.gboxMusic.Text = "Music";
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(878, 15);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(80, 40);
+			this.button4.TabIndex = 1;
+			this.button4.Text = "Browse";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Visible = false;
+			// 
+			// lblPictures
+			// 
+			this.lblPictures.AutoSize = true;
+			this.lblPictures.Location = new System.Drawing.Point(7, 20);
+			this.lblPictures.Name = "lblPictures";
+			this.lblPictures.Size = new System.Drawing.Size(89, 13);
+			this.lblPictures.TabIndex = 0;
+			this.lblPictures.Text = "Pictures Location";
+			// 
+			// gboxPictures
+			// 
+			this.gboxPictures.Controls.Add(this.button4);
+			this.gboxPictures.Controls.Add(this.lblPictures);
+			this.gboxPictures.Location = new System.Drawing.Point(13, 284);
+			this.gboxPictures.Name = "gboxPictures";
+			this.gboxPictures.Size = new System.Drawing.Size(959, 62);
+			this.gboxPictures.TabIndex = 3;
+			this.gboxPictures.TabStop = false;
+			this.gboxPictures.Text = "Pictures";
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(878, 15);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(80, 40);
+			this.button5.TabIndex = 1;
+			this.button5.Text = "Browse";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Visible = false;
+			// 
+			// lblVideo
+			// 
+			this.lblVideo.AutoSize = true;
+			this.lblVideo.Location = new System.Drawing.Point(7, 20);
+			this.lblVideo.Name = "lblVideo";
+			this.lblVideo.Size = new System.Drawing.Size(78, 13);
+			this.lblVideo.TabIndex = 0;
+			this.lblVideo.Text = "Video Location";
+			// 
+			// gboxVideo
+			// 
+			this.gboxVideo.Controls.Add(this.button5);
+			this.gboxVideo.Controls.Add(this.lblVideo);
+			this.gboxVideo.Location = new System.Drawing.Point(13, 352);
+			this.gboxVideo.Name = "gboxVideo";
+			this.gboxVideo.Size = new System.Drawing.Size(959, 62);
+			this.gboxVideo.TabIndex = 2;
+			this.gboxVideo.TabStop = false;
+			this.gboxVideo.Text = "Video";
+			// 
+			// gboxDropbox
+			// 
+			this.gboxDropbox.Controls.Add(this.btnDropbox);
+			this.gboxDropbox.Controls.Add(this.lblDropbox);
+			this.gboxDropbox.Location = new System.Drawing.Point(12, 420);
+			this.gboxDropbox.Name = "gboxDropbox";
+			this.gboxDropbox.Size = new System.Drawing.Size(960, 62);
+			this.gboxDropbox.TabIndex = 3;
+			this.gboxDropbox.TabStop = false;
+			this.gboxDropbox.Text = "Dropbox";
+			// 
+			// btnDropbox
+			// 
+			this.btnDropbox.Location = new System.Drawing.Point(879, 15);
+			this.btnDropbox.Name = "btnDropbox";
+			this.btnDropbox.Size = new System.Drawing.Size(80, 40);
+			this.btnDropbox.TabIndex = 1;
+			this.btnDropbox.Text = "Browse";
+			this.btnDropbox.UseVisualStyleBackColor = true;
+			this.btnDropbox.Visible = false;
+			this.btnDropbox.Click += new System.EventHandler(this.btnDropbox_Click);
+			// 
+			// lblDropbox
+			// 
+			this.lblDropbox.AutoSize = true;
+			this.lblDropbox.Location = new System.Drawing.Point(7, 20);
+			this.lblDropbox.Name = "lblDropbox";
+			this.lblDropbox.Size = new System.Drawing.Size(91, 13);
+			this.lblDropbox.TabIndex = 0;
+			this.lblDropbox.Text = "Dropbox Location";
+			// 
+			// gboxOneDrive
+			// 
+			this.gboxOneDrive.Controls.Add(this.btnOneDrive);
+			this.gboxOneDrive.Controls.Add(this.lblOneDrive);
+			this.gboxOneDrive.Location = new System.Drawing.Point(12, 488);
+			this.gboxOneDrive.Name = "gboxOneDrive";
+			this.gboxOneDrive.Size = new System.Drawing.Size(960, 62);
+			this.gboxOneDrive.TabIndex = 3;
+			this.gboxOneDrive.TabStop = false;
+			this.gboxOneDrive.Text = "OneDrive";
+			// 
+			// btnOneDrive
+			// 
+			this.btnOneDrive.Location = new System.Drawing.Point(879, 15);
+			this.btnOneDrive.Name = "btnOneDrive";
+			this.btnOneDrive.Size = new System.Drawing.Size(80, 40);
+			this.btnOneDrive.TabIndex = 1;
+			this.btnOneDrive.Text = "Browse";
+			this.btnOneDrive.UseVisualStyleBackColor = true;
+			this.btnOneDrive.Visible = false;
+			this.btnOneDrive.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// lblOneDrive
+			// 
+			this.lblOneDrive.AutoSize = true;
+			this.lblOneDrive.Location = new System.Drawing.Point(7, 20);
+			this.lblOneDrive.Name = "lblOneDrive";
+			this.lblOneDrive.Size = new System.Drawing.Size(96, 13);
+			this.lblOneDrive.TabIndex = 0;
+			this.lblOneDrive.Text = "OneDrive Location";
+			// 
+			// btnMove
+			// 
+			this.btnMove.Location = new System.Drawing.Point(390, 560);
+			this.btnMove.Name = "btnMove";
+			this.btnMove.Size = new System.Drawing.Size(160, 40);
+			this.btnMove.TabIndex = 4;
+			this.btnMove.Text = "Move Folder Location";
+			this.btnMove.UseVisualStyleBackColor = true;
+			this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+			// 
+			// btnExit
+			// 
+			this.btnExit.Location = new System.Drawing.Point(891, 559);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(80, 40);
+			this.btnExit.TabIndex = 5;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// rdoDropbox
+			// 
+			this.rdoDropbox.AutoSize = true;
+			this.rdoDropbox.Location = new System.Drawing.Point(6, 19);
+			this.rdoDropbox.Name = "rdoDropbox";
+			this.rdoDropbox.Size = new System.Drawing.Size(65, 17);
+			this.rdoDropbox.TabIndex = 6;
+			this.rdoDropbox.TabStop = true;
+			this.rdoDropbox.Text = "Dropbox";
+			this.rdoDropbox.UseVisualStyleBackColor = true;
+			// 
+			// rdoOneDrive
+			// 
+			this.rdoOneDrive.AutoSize = true;
+			this.rdoOneDrive.Location = new System.Drawing.Point(102, 19);
+			this.rdoOneDrive.Name = "rdoOneDrive";
+			this.rdoOneDrive.Size = new System.Drawing.Size(70, 17);
+			this.rdoOneDrive.TabIndex = 7;
+			this.rdoOneDrive.TabStop = true;
+			this.rdoOneDrive.Text = "OneDrive";
+			this.rdoOneDrive.UseVisualStyleBackColor = true;
+			// 
+			// gboxDropBoxOneDrive
+			// 
+			this.gboxDropBoxOneDrive.Controls.Add(this.lblOR);
+			this.gboxDropBoxOneDrive.Controls.Add(this.rdoOneDrive);
+			this.gboxDropBoxOneDrive.Controls.Add(this.rdoDropbox);
+			this.gboxDropBoxOneDrive.Location = new System.Drawing.Point(13, 556);
+			this.gboxDropBoxOneDrive.Name = "gboxDropBoxOneDrive";
+			this.gboxDropBoxOneDrive.Size = new System.Drawing.Size(335, 52);
+			this.gboxDropBoxOneDrive.TabIndex = 8;
+			this.gboxDropBoxOneDrive.TabStop = false;
+			// 
+			// lblOR
+			// 
+			this.lblOR.AutoSize = true;
+			this.lblOR.Location = new System.Drawing.Point(73, 21);
+			this.lblOR.Name = "lblOR";
+			this.lblOR.Size = new System.Drawing.Size(23, 13);
+			this.lblOR.TabIndex = 8;
+			this.lblOR.Text = "OR";
+			// 
+			// frmMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(984, 611);
+			this.Controls.Add(this.gboxDropBoxOneDrive);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.btnMove);
+			this.Controls.Add(this.gboxOneDrive);
+			this.Controls.Add(this.gboxDropbox);
+			this.Controls.Add(this.gboxVideo);
+			this.Controls.Add(this.gboxPictures);
+			this.Controls.Add(this.gboxMusic);
+			this.Controls.Add(this.gboxDownloads);
+			this.Controls.Add(this.gboxDocuments);
+			this.Controls.Add(this.gboxDesktop);
+			this.MaximizeBox = false;
+			this.Name = "frmMain";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Move Folder to Another Location";
+			this.gboxDesktop.ResumeLayout(false);
+			this.gboxDesktop.PerformLayout();
+			this.gboxDocuments.ResumeLayout(false);
+			this.gboxDocuments.PerformLayout();
+			this.gboxDownloads.ResumeLayout(false);
+			this.gboxDownloads.PerformLayout();
+			this.gboxMusic.ResumeLayout(false);
+			this.gboxMusic.PerformLayout();
+			this.gboxPictures.ResumeLayout(false);
+			this.gboxPictures.PerformLayout();
+			this.gboxVideo.ResumeLayout(false);
+			this.gboxVideo.PerformLayout();
+			this.gboxDropbox.ResumeLayout(false);
+			this.gboxDropbox.PerformLayout();
+			this.gboxOneDrive.ResumeLayout(false);
+			this.gboxOneDrive.PerformLayout();
+			this.gboxDropBoxOneDrive.ResumeLayout(false);
+			this.gboxDropBoxOneDrive.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox gboxDesktop;
+		private System.Windows.Forms.Button Browse;
+		private System.Windows.Forms.Label lblDesktop;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label lblDocuments;
+		private System.Windows.Forms.GroupBox gboxDocuments;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label lblDownloads;
+		private System.Windows.Forms.GroupBox gboxDownloads;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label lblMusic;
+		private System.Windows.Forms.GroupBox gboxMusic;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label lblPictures;
+		private System.Windows.Forms.GroupBox gboxPictures;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Label lblVideo;
+		private System.Windows.Forms.GroupBox gboxVideo;
+		private System.Windows.Forms.GroupBox gboxDropbox;
+		private System.Windows.Forms.Button btnDropbox;
+		private System.Windows.Forms.Label lblDropbox;
+		private System.Windows.Forms.GroupBox gboxOneDrive;
+		private System.Windows.Forms.Button btnOneDrive;
+		private System.Windows.Forms.Label lblOneDrive;
+		private System.Windows.Forms.Button btnMove;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.RadioButton rdoDropbox;
+		private System.Windows.Forms.RadioButton rdoOneDrive;
+		private System.Windows.Forms.GroupBox gboxDropBoxOneDrive;
+		private System.Windows.Forms.Label lblOR;
+	}
+}
+
---
+++ GKMoveFolders/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GKMoveFolders
+{
+	public partial class frmMain : Form
+	{
+
+		bool selectFolderDropBox = false;
+		bool selectFolderOneDrive = false;
+
+		public frmMain()
+		{
+			InitializeComponent();
+
+
+
+			lblDesktop.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+			lblDocuments.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			//lblDownloads.Text = Environment.GetFolderPath(Environment.SpecialFolder.Download);
+			var pathDownload = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+			lblDownloads.Text = pathDownload;
+			lblMusic.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+			lblPictures.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+			lblVideo.Text = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+			
+			try
+			{
+				var pathDropBox = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Dropbox\\info.json");
+
+				if (File.Exists(pathDropBox))
+				{
+					string stpath = File.ReadAllText(pathDropBox);
+					lblDropbox.Text = "Please browse for the Location you want to store your folders in!";
+					btnDropbox.Visible = true;
+				}
+			}
+
+			catch (Exception)
+
+			{
+
+				lblDropbox.Text = "There is no Dropbox Location!";
+
+			}
+
+			try
+
+			{
+				var pathOneDrive = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Microsoft\\OneDrive\\settings\\Business1");
+
+				if (Directory.Exists(pathOneDrive))
+				{
+					lblOneDrive.Text = "Please browse for the Location you want to store your folders in!";
+					btnOneDrive.Visible = true;
+					//var pathOneDrive = Path.Combine(Environment.GetFolderPath(Environment.GetFolderPath(), "Dropbox\\info.json");
+				}
+			}
+
+			catch (Exception)
+
+			{
+
+				lblOneDrive.Text = "There is no OneDrive Location!";
+
+			}
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void btnDropbox_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+			//fbd.RootFolder = Environment.SpecialFolder.UserProfile;
+
+			if (fbd.ShowDialog() == DialogResult.OK)
+			{
+				lblDropbox.Text = fbd.SelectedPath;
+				selectFolderDropBox = true;
+				rdoDropbox.Checked = true;
+				rdoDropbox.Select();
+			}
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog fbd = new FolderBrowserDialog();
+			//fbd.RootFolder = Environment.SpecialFolder.UserProfile;
+
+			if (fbd.ShowDialog() == DialogResult.OK)
+			{
+				lblOneDrive.Text = fbd.SelectedPath;
+				selectFolderOneDrive = true;
+				rdoOneDrive.Checked = true;
+				rdoOneDrive.Select();
+			}
+		}
+
+		private void btnMove_Click(object sender, EventArgs e)
+		{
+			if (!rdoDropbox.Checked && !rdoOneDrive.Checked)
+			{
+				MessageBox.Show("Please Choose a path to either Dropbox or OneDrive");
+			}
+
+			if (rdoDropbox.Checked || selectFolderDropBox)
+			{
+				MessageBox.Show("Please Select a DropBox Location!");
+				rdoDropbox.Checked = false;
+			}
+
+			if (rdoOneDrive.Checked || selectFolderOneDrive)
+			{
+				MessageBox.Show("Please Select a OneDrive Location!");
+				rdoOneDrive.Checked = false;
+			}
+		}
+	}
+}
